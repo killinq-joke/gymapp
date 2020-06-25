4a65a021487bd1a18a482f52973277ef26efd14d
zakaria touzri <zakadmaster@gmail.com>
2020-06-25 23:03:30 +0200

    Addition of footer routes & links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
 import "./App.css";
+
+import Workout from "./components/Workout";
+import Nutrition from "./components/Nutrition";
+import Premium from "./components/Premium";
+import Account from "./components/Account";
 
 function App() {
   return (
     <div className="App">
-      
+      <Route path="/workout">
+        <Workout />
+      </Route>
+      <Route path="/nutrition">
+        <Nutrition />
+      </Route>
+      <Route path="/premium">
+        <Premium />
+      </Route>
+      <Route path="/account">
+        <Account />
+      </Route>
       <footer className="Bottom">
-        <a>workout</a>
-        <a>nutrition</a>
-        <a>premium</a>
-        <a>account</a>
+        <Link to="/workout">workout</Link>
+        <Link to="/nutrition">nutrition</Link>
+        <Link to="/premium">premium</Link>
+        <Link to="/account">account</Link>
       </footer>
     </div>
   );
