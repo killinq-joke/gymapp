28bd56a53c6e2bf7fb263840b799aab61b8d7604
zakaria touzri <zakadmaster@gmail.com>
2020-06-26 01:47:02 +0200

    Addition of cart

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Icon, InlineIcon } from "@iconify/react";
+import magnifyingGlass from "@iconify/icons-entypo/magnifying-glass";
+import powerman from "../imgs/powerman.jpg";
 
 export default function Workout() {
-  return <></>;
+  return (
+    <>
+      <h3>TRAIN LIKE ROBERT BLAZEVIC</h3>
+      <div className="exercises">
+        <h1>Exercises</h1>
+        <Icon icon={magnifyingGlass} width="30" height="30" />
+      </div>
+      <div style={{ backgroundImage:`url(${powerman})`, "background-size": "200px", height: "100px" }}>
+        <button>salut</button>
+      </div>
+    </>
+  );
 }
