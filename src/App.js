aeef151be9f52e8917e9d8ef4baf645ffaead78f
zakaria touzri <zakadmaster@gmail.com>
2020-06-25 22:25:33 +0200

    Addition of base footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="Bottom">
+        <a>workout</a>
+        <a>nutrition</a>
+        <a>premium</a>
+        <a>account</a>
+      </div>
     </div>
   );
 }
