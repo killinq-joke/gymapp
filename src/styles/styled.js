821acdff8d8a606b4fcfb0b7992116ac61751819
zakaria touzri <zakadmaster@gmail.com>
2020-06-26 13:16:44 +0200

    Addition of Cards & Card styled components

---
+++ src/styles/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import powerman from "../imgs/powerman.jpg";
+
+export const Cards = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  margin-bottom: 15vh;
+`;
+
+export const Card = styled.div`
+  border-radius: 15px;
+  height: 20vh;
+  width: 85%;
+  margin-bottom: 3%;
+`;
