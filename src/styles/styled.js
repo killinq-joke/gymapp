47119f951239958f0174c250702f752569f9f999
zakaria touzri <zakadmaster@gmail.com>
2020-06-26 14:02:30 +0200

    Addition of Try styling

--- src/styles/styled.js
+++ src/styles/styled.js
@@ -11,13 +11,19 @@ export const Card = styled.div`
   border-radius: 15px;
   height: 20vh;
   width: 85%;
-  margin-bottom: 3%;
-`;
-
-export const Title = styled.h2`
+  margin-bottom: 5%;
   color: hsla(0, 0%, 100%, 0.87);
 `;
 
+export const Title = styled.h2`
+  margin-left: 5%;
+`;
+
 export const Try = styled.button`
+  border: 0;
+  border-radius: 30px;
+  padding: 5px 20px;
+  font-weight: bolder;
   background-color: rgba(76, 217, 100);
+  cursor: pointer;
 `;
