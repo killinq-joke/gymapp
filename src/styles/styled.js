4a506dfbf9e2d61596cd4217adf8028740f6bf06
zakaria touzri <zakadmaster@gmail.com>
2020-06-28 12:14:29 +0200

    Addition of styled P & Ps

--- src/styles/styled.js
+++ src/styles/styled.js
@@ -7,13 +7,17 @@ export const Cards = styled.div`
   margin-bottom: 15vh;
 `;
 
-export const Card = styled.div`
-  border-radius: 15px;
-  height: 20vh;
-  width: 85%;
-  margin-bottom: 5%;
-  color: hsla(0, 0%, 100%, 0.87);
-`;
+export const Card = styled.a({
+  "text-align": "left",
+  "border-radius": "15px",
+
+  cursor: "pointer",
+  height: "20%",
+  width: "85%",
+  "margin-bottom": "5%",
+  color: "hsla(0, 0%, 100%, 0.87)",
+  
+});
 
 export const Title = styled.h2`
   margin-left: 5%;
@@ -22,8 +26,16 @@ export const Title = styled.h2`
 export const Try = styled.button`
   border: 0;
   border-radius: 30px;
-  padding: 5px 20px;
+  padding: 6px 6%;
   font-weight: bolder;
   background-color: rgba(76, 217, 100);
+  margin: 0 5% 10px 0;
   cursor: pointer;
+`;
+
+export const P = styled.p`
+  font-size: 0.7rem;
+`;
+export const Ps = styled.div`
+  margin-left: 5%;
 `;
