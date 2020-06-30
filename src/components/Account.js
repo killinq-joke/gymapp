7dee2bc3fb27d41998f466c7cd21ef111c55d710
zakaria touzri <zakadmaster@gmail.com>
2020-06-30 18:28:16 +0200

    Addition of height slider

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Icon } from '@iconify/react';
+import arrowLeftShort from '@iconify/icons-bi/arrow-left-short';
+
+import { GenderButton } from "../styles/styled";
 
 export default function Account() {
-  return <></>;
+  const [height, setHeight] = useState(150)
+  const name = "Mark Summerby";
+
+
+  return (
+    <>
+    <Icon style={{margin: "5% 0 0 2%"}} icon={arrowLeftShort} width="40" height="40"/>
+      <h1 style={{marginLeft: "5%"}}>{name}</h1>
+      <div style={{ display: "flex", flexFlow: "column" }}>
+        <h2 style={{marginLeft: "5%"}}>Gender</h2>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <GenderButton>
+            <img alt="" /> Male
+          </GenderButton>
+          <GenderButton>
+            <img alt=""/>
+            Female
+          </GenderButton>
+        </div>
+      </div>
+
+      <div><p>My Height</p><p>{height}cm</p><input type="range" min="100" max="250" onChange={e => setHeight(e.target.value)}/></div>
+    </>
+  );
 }
